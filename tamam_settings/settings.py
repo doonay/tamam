0c697c85786f7d89dcd475745aee3a6f6d4083de
dementiado <dementiado@gmail.com>
2023-03-31 16:13:36 +0300

    security settings with ini file

--- tamam_settings/settings.py
+++ tamam_settings/settings.py
@@ -1,4 +1,4 @@
-from .settings import config
+import configparser
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+config = configparser.ConfigParser()
+config.read('example.ini')
+config.sections()
+SECRET_KEY = config['DEFAULT']['SECRET_KEY']
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
