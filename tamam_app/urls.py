fe11e135c4e4bf6c4e6af00a3c2a44f8c09f51e2
dementiado <dementiado@gmail.com>
2023-06-01 21:27:22 +0300

    new init

--- tamam_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    #path('payment/<str:title>/', views.payment, name='payment'),
-    path('payment/<str:title>/', views.show_game, name='payment'),
-]
