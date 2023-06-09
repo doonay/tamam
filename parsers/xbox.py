8ce8efe054d54388e3f2b3f48cbef3b38f9e8ce4
dementiado <dementiado@gmail.com>
2023-06-09 17:59:55 +0300

    Hастроил парсер, почистил проект, парсер отрабатывает как надо

---
+++ parsers/xbox.py
@@ -0,0 +1,49 @@
+import asyncio
+import aiohttp
+from tamam_logger import tamam_logger
+from insert_games import db_insert
+import time
+import requests
+
+from xbox_insert_logic import insert_logic
+
+def get_games_ids() -> list: # список айдишников всех игр
+    url = 'https://catalog.gamepass.com/sigls/v2?id=f6f1f99f-9b49-4ccd-b3bf-4d9767a77f5e&language=ru-ru&market=RU'
+    session = requests.Session()
+    r = session.get(url)
+    games_ids_list = [id_dict['id'] for id_dict in r.json()[1:]]
+    #games_ids_list = [r.json()[1:][1]['id'], r.json()[1:][2]['id']]
+    return games_ids_list
+
+async def get_product_data(session, chunks_str): #вот эти корутины запускаются параллельно
+    url = f'https://displaycatalog.mp.microsoft.com/v7.0/products?bigIds={chunks_str}&market=RU&languages=RU-RU&MS-CV=DGU1mcuYo0WMMp+F.1'
+    async with session.get(url) as response:
+        product_data = await response.json()
+    return product_data['Products']
+
+async def parser():
+    games_ids_list = get_games_ids()
+    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(limit=20)) as session:
+        tasks = []
+        chunk_size = 10  # Количество идентификаторов в пакете
+        id_chunks = [games_ids_list[i:i + chunk_size] for i in range(0, len(games_ids_list), chunk_size)]
+        for chunk in id_chunks:
+            tasks.append(get_product_data(session, ','.join(chunk)))
+        product_results = await asyncio.gather(*tasks)
+        all_redata_games = []
+        for all_pages_item in product_results:
+            for one_page_item in all_pages_item:
+                one_redata_game = insert_logic(one_page_item)
+                all_redata_games.append(one_redata_game)
+        return all_redata_games    
+
+def main():
+    import os
+    table_name = os.path.basename(__file__).split('.')[0]
+    tamam_logger("DEBUG", f"Задано имя таблицы {table_name}")
+    all_redata_games = asyncio.run(parser())
+    db_insert(table_name, all_redata_games)
+    
+if __name__ == '__main__':
+    main()
+    tamam_logger("DEBUG", "-----")
---
+++ parsers/xbox_insert_logic.py
@@ -0,0 +1,32 @@
+def insert_logic(one_product):
+    #all_pages_items = []
+    #for all_pages_item in product_results: 
+    #    for one_page_item in all_pages_item:
+    redata_dict = {}
+
+    redata_dict["product_id"] = one_product["ProductId"]
+    redata_dict["title"] = one_product["LocalizedProperties"][0]["SortTitle"]
+    # Пока жестко прописываю платформу, ибо заведомо знаю, что это икс бокс
+    redata_dict["platforms"] = ['XBOX']
+    try:
+        base_price = int(one_product["DisplaySkuAvailabilities"][0]["Availabilities"][0]["OrderManagementData"]["Price"]["ListPrice"] * 100)
+        redata_dict["base_price"] = base_price
+    except:
+        base_price = 0
+    try:    
+        discounted_price = int(one_product["DisplaySkuAvailabilities"][0]["Availabilities"][0]["OrderManagementData"]["Price"]["WholesalePrice"])
+        redata_dict["discounted_price"] = discounted_price
+        redata_dict["discount"] = int((base_price - discounted_price) / (base_price / 100))  # back percentage
+    except:
+        redata_dict["discounted_price"] = 0
+        redata_dict["discount"] = 0
+    try:
+        # Квадратная картинка, если вдруг понадобится, сейчас используем прямоугольную с надписью
+        # redata_dict["image_square"] = f'https:{one_page_item["LocalizedProperties"][0]["Images"][1]["Uri"]}'
+        redata_dict["img"] = f'https:{one_product["LocalizedProperties"][0]["Images"][6]["Uri"]}' # + '?h=60&format=jpg'
+    except:
+        redata_dict["img"] = None
+        #!!! продумать изображение по умолчанию, если парсер не нашел!!!
+    #all_pages_items.append(redata_dict)
+    #return all_pages_items
+    return redata_dict
